7f3fa42cb40f8f6a3b396c59d4412a84c0b10731
Clemente <jopclem@gmail.com>
2024-02-06 12:29:56 +0000

    Adds global configuration

---
+++ app/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    database_hostname: str
+    database_port: str
+    database_password: str
+    database_name: str
+    database_username: str
+    test_database_name: str
+
+    jwt_secret_key: str
+    jwt_algorythm: str
+    jwt_access_token_expire_minutes: int
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
